829980769524a3214136641113033d762f5d92ec
Marcos Sena <marcos.sena@mtsolutions.com>
2026-02-02 14:08:58 -0400

    configuracion de la bd

---
+++ BackEnd/GetResumeCounter.cs
@@ -0,0 +1,64 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend
+{
+    public class GetResumeCounter
+    {
+        private readonly ILogger<GetResumeCounter> _logger;
+
+        public GetResumeCounter(ILogger<GetResumeCounter> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("GetResumeCounter")]
+        public MultiResponse Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req,
+            [CosmosDBInput(
+                databaseName: "AzureResume",
+                containerName: "Counter",
+                Connection = "CosmosDbConnectionString",
+                Id = "1",
+                PartitionKey = "1")] Counter? input)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            // Si no existe, creamos uno nuevo
+            if (input == null)
+            {
+                input = new Counter { Id = "1", Count = 0 };
+            }
+
+            // Aumentamos contador
+            input.Count += 1;
+
+            return new MultiResponse()
+            {
+                SavedCounter = input,
+                HttpResponse = new OkObjectResult(input)
+            };
+        }
+    }
+
+    public class MultiResponse
+    {
+        [CosmosDBOutput(
+            databaseName: "AzureResume",
+            containerName: "Counter",
+            Connection = "CosmosDbConnectionString",
+            CreateIfNotExists = true)]
+        public Counter? SavedCounter { get; set; }
+
+        [HttpResult]
+        public IActionResult? HttpResponse { get; set; }
+    }
+
+    public class Counter
+    {
+        public string? Id { get; set; }
+        public int Count { get; set; }
+    }
+}
